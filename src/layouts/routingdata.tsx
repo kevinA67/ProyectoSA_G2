876f4a6c6a88ff344e9e5239e1558a85813c9b8f
Jrapalo07 <138176649+Jrapalo07@users.noreply.github.com>
2024-12-14 00:44:07 -0600

    Graficas agregadas a pantalla home

--- src/layouts/routingdata.tsx
+++ src/layouts/routingdata.tsx
@@ -3,11 +3,11 @@ import Login from '../pages/authentication/Login';
 import SingUp from '../pages/authentication/SingUp';
 import Home from '../pages/game/Home';
 
-
 export const Routingdata = [
   //Pages
   { path: `/`, element: <Login/> },
   { path: `/home`, element: <Home /> },
   { path: `/tictactue`, element: <Tablero /*socket={socket}*/ /> },
   { path: `/singup`, element: <SingUp /> },
+
 ];
